1df6f87a2bae5ca678b406793f01330dda80f7a7
Anthony Campbell <anthony-campbell@hotmail.com>
2022-09-29 14:36:54 -0700

    Extract admin parent class functionality.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,4 @@
-class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN_USER"], password: ENV["ADMIN_PASS"]
+class Admin::DashboardController < Admin::AdminController
   def show
     @product_count = Product.all.count
     @category_count = Category.all.count
